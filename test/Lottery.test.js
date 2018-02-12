11ce110bf1f60385e9cc0a7e9a83cfa9c9648a02
Jacky Tang <jackytck@gmail.com>
2018-02-13 00:04:51 +0800

    :white_check_mark: add test: send money to winner and reset itself

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -94,4 +94,22 @@ describe('Lottery Contract', () => {
     }
     assert(false)
   })
+
+  it('sends money to the winner and resets the players array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    })
+
+    const initialBalance = await web3.eth.getBalance(accounts[0])
+    await lottery.methods.pickWinner().send({ from: accounts[0] })
+    const finalBalance = await web3.eth.getBalance(accounts[0])
+    const difference = finalBalance - initialBalance
+    const players = await lottery.methods.getPlayers().call({ from: accounts[0] })
+    const lotteryBalance = await web3.eth.getBalance(lottery.options.address)
+
+    assert(difference > web3.utils.toWei('1.8', 'ether'))
+    assert.equal(players.length, 0)
+    assert.equal(lotteryBalance, 0)
+  })
 })
